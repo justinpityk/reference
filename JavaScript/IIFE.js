99b6d0df5a510fceab453eabc061c9259310cd57
Justin Pityk <justin.pityk@philips.com>
2020-05-29 12:35:14 -0400

    added more to IIFE.js

--- JavaScript/IIFE.js
+++ JavaScript/IIFE.js
@@ -1,2 +1,7 @@
 
-//IIFE
+//IIFE (Immediately Invoked Function Expression) is a function that runs as soon as it is defined.
+// - it is a design pattern that is also known as a Self-Executing Anonymous Function
+
+(function() {
+    //statements
+})();
